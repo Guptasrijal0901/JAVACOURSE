c5797a831a9b960621c1ad24b10c71d9a93cd9ac
Srijal Gupta <guptasrijal0901@gmail.com>
2024-01-25 16:05:53 +0530

    test

--- pattern.java
+++ pattern.java
@@ -3,12 +3,49 @@ import java.util.Scanner;
 public class pattern {
     public static void main (String arg[]){
     Scanner sc = new Scanner(System.in);
-    for (int line = 1 ; line <= 4 ; line++){
-        for ( int star = 1 ; star <= line ; star++){
-            System.out.print(" * ");
+    // System.out.print("Enter n : ");
+    // int n = sc.nextInt() ;
+    // for (int line = 1 ; line <= n ; line++){
+    //     for ( int star = 1 ; star <= line ; star++){
+    //         System.out.print(" * ");
+    //     }
+    //     System.out.println();
+    // }
+    // System.out.print("Enter n : ");
+    // int n = sc.nextInt() ;
+    // for (int line = 1 ; line <= n ; line++){
+    //         for ( int star = 1 ; star <= (n-line+1) ; star++){
+    //             System.out.print(" * ");
+    //         }
+    //         System.out.println();
+    //     }
+    // System.out.print("Enter n : ");
+    // int n = sc.nextInt() ;
+    // for ( int i = 1 ; i <= n ; i++){
+    //     for ( int j = 1 ; j <= (n-i+1) ; j++){
+    //         System.out.print(i);
+    //     }
+    //     System.out.println();
+    // }
+    // System.out.print("Enter n : ");
+    // int n = sc.nextInt() ;
+    // for ( int i = 1 ; i <= n ; i++){
+    //     for ( int j = 1 ; j <= i ; j++){
+    //         System.out.print(i);
+    //     }
+    //     System.out.println();
+    // }
+    System.out.print("Enter n : ");
+    int n = sc.nextInt() ;
+    char ch = 'A' ;
+    for (int i = 1 ; i <= n ; i++){
+        for (int j = 1 ; j <= i ; j++){
+            System.out.print( ch + " " );
+            ch++;
         }
         System.out.println();
     }
     sc.close();
     }
 }
+// phele code row k liye fir col k liye 
