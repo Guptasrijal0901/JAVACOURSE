704cc32ca64b24f2797aeb59a55a91af834181e1
Srijal Gupta <guptasrijal0901@gmail.com>
2024-01-30 01:03:52 +0530

    test

---
+++ array.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class array {
+     public static void main (String arg[]){
+    Scanner sc = new Scanner(System.in);
+    sc.close();
+    }
+    
+}
+// list of elements of the same type placed in a contiguous memory location 
+//operation in array 
+//create
+//input 
+//output
+//update
+//syntax
+//datatype arrayname[] = new datatype[size]
