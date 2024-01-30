63a1cf8735806f2e2090153ce349d4aa0bf6d279
Srijal Gupta <guptasrijal0901@gmail.com>
2024-01-30 18:12:57 +0530

    srijal

--- linearSearchARRAY.java
+++ linearSearchARRAY.java
@@ -2,22 +2,24 @@ import java.util.Scanner;
 
 public class linearSearchARRAY {
     public static void birthday(int day[], int key ){
-        boolean found = true;
+        boolean found = false; // to check true or false 
         for (int i = 0 ; i < day.length ; i++){
         if (day[i] == key) {
             System.out.println(i);
             found = true;
             break;
-        }if (!found) {
-            System.out.println("NOT FOUND ");
         }
         }
+        if (!found) {
+        System.out.println("NOT FOUND ");
         }
+        }
+        
     public static void main (String arg[]){
       
     Scanner sc = new Scanner(System.in);
     int day [] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    int key = 9 ;
+    int key = 20 ;
     birthday(day, key);
     sc.close();
     }
