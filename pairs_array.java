0bb10e65c2f17276c1a88e64241f64377f80a399
Srijal Gupta <guptasrijal0901@gmail.com>
2024-02-04 01:54:29 +0530

    op

---
+++ pairs_array.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class pairs_array {
+    public static void pairUp(int n []){
+        int m = n.length;
+        for(int i = 0 ; i < n.length ; i++){
+           for(int j = i+1 ; j < n.length; j++ ){ 
+// i ki jo bhi value h j ki usse ek zayada hona chahiye 
+// taaki vo sirf apne aage wale se hi pair kare
+            System.out.print("("+ n[i] + " , " + n[j]+ ")");
+           }System.out.println();
+        }
+        int total_pairs = (m*(m-1))/2;
+        System.out.print("Total number of pairs we get is :"+total_pairs);
+    }
+    public static void main (String arg[]){
+    Scanner sc = new Scanner(System.in);
+    int n []= {2, 4 , 6, 8, 10};
+    pairUp(n);
+    sc.close();
+    }
+}
--- reverse_an_array.java
+++ reverse_an_array.java
@@ -1,8 +1,35 @@
 import java.util.Scanner;
 
 public class reverse_an_array {
+    public static void reverse(int n []){
+        // int m = n.length;
+        // for (int i = 0 ; i <= m/2 ; i++){
+        //     int temp = n[i];
+        //     n[i]= n[m-i-1];
+        //     n[m-i-1] = temp;
+        // }
+        // System.out.print("Reverse of the array is : " );
+        // for (int i = 0 ; i < m ; i++){
+        //     System.out.print( n[i]+ " ");
+        // }
+        // next method 
+        int first = 0 ;
+        int last = n.length-1;
+        while (first < last ) {
+            int temp = n[last];
+            n[last]= n[first];
+            n[first]= temp;
+            first++;
+            last--;
+        }
+    }
  public static void main (String arg[]){
     Scanner sc = new Scanner(System.in);
+    int n []= {9, 8, 7, 6, 5, 4, 3, 2, 1};
+    reverse(n);
+    for (int i = 0 ; i < n.length; i++ ){
+        System.out.print(n[i]+ " ");
+    }
     sc.close();
     } 
 }
