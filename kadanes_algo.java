4968bdf585dff4833c657f2fe20cbae3ea88fea9
Srijal Gupta <guptasrijal0901@gmail.com>
2024-02-05 01:12:01 +0530

    lol

---
+++ kadanes_algo.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class kadans_algo {
+     public static void main (String arg[]){
+    Scanner sc = new Scanner(System.in);
+    sc.close();
+    }
+}
--- prefix_array.java
+++ prefix_array.java
@@ -2,22 +2,22 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class prefix_array {
-    public static void prefixMethod(int array[]){
+    public static void findMaxMInSubarraySum(int array[]){
     int curSum = 0 ;
     int maxSum = Integer.MIN_VALUE;
     int minSum = Integer.MAX_VALUE;
         int prefixArray[]= new int [array.length];
-        int prefixSum = 0 ;
         for (int i = 0 ; i < array.length ; i++){
+        int prefixSum = 0 ;
         prefixSum = array[i] + prefixSum;
         prefixArray[i] = prefixSum;
         }
-        System.out.println("Prefix of "+ Arrays.toString(array) + " is "+ Arrays.toString(prefixArray));
+        System.out.println("Prefix array of "+ Arrays.toString(array) + " is "+ Arrays.toString(prefixArray));
         // array ko print karwana ho toh 
 
         for (int i = 0 ; i < array.length ; i++){
             int start = i;
-            for (int j = i+1 ; j < array.length; j++){
+            for (int j = i ; j < array.length; j++){
             int end = j ;
             curSum = start == 0 ? prefixArray[end] : prefixArray[i] - prefixArray[start - 1 ] ;
             // if (maxSum < curSum){
@@ -37,7 +37,7 @@ public class prefix_array {
      public static void main (String arg[]){
     Scanner sc = new Scanner(System.in);
     int array [] = {1, -2, 6, -1, 3};
-    prefixMethod(array);
+    findMaxMInSubarraySum(array);
     sc.close();
     }
 }
