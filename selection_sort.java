dcb94d163b7f038ef7a2b5c871f9fa2ae2359d5a
Srijal Gupta <guptasrijal0901@gmail.com>
2024-02-11 23:06:32 +0530

    lol

---
+++ selection_sort.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class selection_sort {
+    public static void main (String arg[]){
+    Scanner sc = new Scanner(System.in);
+    sc.close();
+    }
+}
+// IDEA : PICK THE SMALLEST (FROM UNSORTED), PUT IT AT THE BEGINNING
+// ESME SARE ELEMENT MAI SE SMALLEST NIKAL LETE HAI OR USKO AAGE PLACE KAR DETE HAI 
+// FIR REST ELEMENT KO COMPARE KARTE HAI OR USME SE SABSE CHOOTE KO AAGE PLACE KAR DETE HAI 
+// ESME BUBBLE KI TRAH HAMESHA SWAP KAENE KI ZARURAT NHI HAI 
+// 
