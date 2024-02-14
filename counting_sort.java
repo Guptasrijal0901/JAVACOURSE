bea84863818db9a58516b4b3f88db691dca6167d
Srijal Gupta <guptasrijal0901@gmail.com>
2024-02-14 20:07:11 +0530

    lol

--- counting_sort.java
+++ counting_sort.java
@@ -1,8 +1,44 @@
 import java.util.Scanner;
 
 public class counting_sort {
+    public static void countingSort(int array[] ){
+        // yaha pe hm largest value find karenge till that index 
+        int n = array.length;
+        // int largest = Integer.MIN_VALUE;
+        // for (int i = 0 ; i < n; i++){
+        // largest = Math.max(largest, array[i]);
+        // // System.out.println("largest till index "+ i +" is "+ largest);
+        // }
+        // yaha pe aab hm new array banayenge or usme frequency of number store karenge 
+        // count array count karege ki hmhara frequency count karta ha kon sa number kitni baar hai 
+        // essi wajha se range hmara esme choota hi hota hai 
+    int count[] = new int [n]; // ek naya array bana liye 
+    for (int i = 0 ; i <array.length; i++){
+    count [array[i]]++;
+    }
+    // sorting 
+    // wahi jab frequency jab 1 se zayfda hai toh ek baar store karte hai or uske baad decrease by 1 kar dete hai 
+    int j = 0 ;
+    for (int i = 0; i < count.length; i++ ){
+        while(count[i] > 0 ){
+            array[j] = i ;
+            j++; // position hai aage jane k liye 
+            count[i]--; // count array mai decrease karne k liye ki yeh value ek baar store ho gyi aab esko ek decrease kar do
+        }
+    }
+    }
+    public static void printarray(int array[]){
+        int m = array.length;
+        for ( int i = 0 ; i < m ; i++){
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
     public static void main (String arg[]){
         Scanner sc = new Scanner(System.in);
+        int array [] = {1, 4, 5, 2, 5, 7, 3, 2, 5, 7, 0};
+        countingSort(array);
+        printarray(array);
         sc.close();
         }
 }
---
+++ tempCodeRunnerFile.java
@@ -0,0 +1,1 @@
+        // System.out.println("largest till index "+ i +" is "+ largest);
