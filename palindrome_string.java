4998fc005f3d494bac5feffdc40ac680f790090c
Srijal Gupta <guptasrijal0901@gmail.com>
2024-02-19 19:35:00 +0530

    lol

--- palindrome_string.java
+++ palindrome_string.java
@@ -1,9 +1,26 @@
 import java.util.Scanner;
 
 public class palindrome_string {
+    public static boolean isPalindrome(String str){
+    int n = str.length(); 
+    for (int i = 0; i < n/2 ; i++) {
+        if ( str.charAt(i) != str.charAt(n-1-i)) {
+            return false;
+        }
+    }
+    return true;
+    }
     public static void main (String arg[]){
     Scanner sc = new Scanner(System.in);
+    String str = "racecar";
+    // isPalindrome(str);
+    if (isPalindrome(str)) {
+        System.out.println("It is palindrome");
+    }else{
+        System.out.println("It is not palindrome");
+    }
     sc.close();
     }
     
 }
+// Tc of this code is O(n) 
