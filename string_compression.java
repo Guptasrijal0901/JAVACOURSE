4c6be473679d796f51bdeafc2503fc51454b4899
Srijal Gupta <guptasrijal0901@gmail.com>
2024-03-02 15:04:34 +0530

    lol

--- string_compression.java
+++ string_compression.java
@@ -1,3 +1,8 @@
+import java.util.Scanner;
+
 public class string_compression {
-    
+    public static void main (String arg[]){
+        Scanner sc = new Scanner(System.in);
+        sc.close();
+        }
 }
