5abc1958f9c508aeff6253b0979f4ace352aca79
Srijal Gupta <guptasrijal0901@gmail.com>
2024-03-09 00:00:37 +0530

    lol

--- oops_constructor.java
+++ oops_constructor.java
@@ -1,3 +1,17 @@
 public class oops_constructor {
-    
+    public static void main(String[] args) {
+        student s1 = new student("Srijal Gupta");
+        System.out.println(s1.name);
+    }
+}
+class student {
+    String name ;
+    int age ;
+    int roll ;
+
+    // this is constructor
+    student( String name ){  
+        this.name = name;
+        System.out.println("Constructor is called");
+    }
 }
