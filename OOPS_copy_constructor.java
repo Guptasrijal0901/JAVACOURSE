055645335864d755e6ff0ab24f856d366236e3a1
Srijal Gupta <guptasrijal0901@gmail.com>
2024-03-21 01:42:08 +0530

    lol

--- OOPS_copy_constructor.java
+++ OOPS_copy_constructor.java
@@ -3,17 +3,24 @@ public class OOPS_copy_constructor {
       Student_data s1 = new Student_data();
       s1.name = "Srijal Gupta" ;
       s1.roll = 180 ;
+      s1.marks = new int[3];
       s1.password = "xyz";
-// aab student apna password bhul gya hai toh hm naya constructor banayenge 
-// jisme student ka alag alag arguments honge jaise name course roll or age 
+      s1.marks[0]= 98;
+      s1.marks[1]= 99;
+      s1.marks[2]= 89;
 // Create a new Student_data object using copy constructor    
     Student_data s2 = new Student_data(s1);
     // Set new password for s2
-    s2.password = "xyz";
+    s2.password = "change_xyz";
+    s1.marks[1] = 97; 
+    // after coping it is changed now the new marks will shown not old one
     // Print details of s2
     System.out.println("Name: " + s2.name);
     System.out.println("Roll: " + s2.roll);
     System.out.println("Password: " + s2.password);
+    for(int i = 0 ; i < 3 ; i++){
+    System.out.println("Marks "+  i +" = "+ s2.marks[i]);
+    }
     }
 }
 class Student_data{
@@ -27,6 +34,7 @@ Student_data(Student_data s1){
    this.roll = s1.roll ;
    marks = new int[3];
    this.marks = s1.marks;
+   this.password = s1.password;
    }
 // yaha pe aab constructors design karte hai 
 // Default constructor
