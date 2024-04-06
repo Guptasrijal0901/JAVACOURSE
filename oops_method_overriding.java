7cc28f672e2f8afad557e967751b9c58b10951b3
Srijal Gupta <guptasrijal0901@gmail.com>
2024-04-06 18:37:52 +0530

    lol

--- oops_method_overriding.java
+++ oops_method_overriding.java
@@ -1,4 +1,17 @@
 public class oops_method_overriding {
-    
+    public static void main(String[] args) {
+    Deer D = new Deer();
+    D.eat();
+    }
 }
 // parent and child classes both contain the same function with a different definition.
+class Animal{
+    void eat(){
+        System.out.println("eats anything");
+    }
+}
+class Deer extends Animal{
+    void eat(){
+        System.out.println("Eats grass");
+    }
+}
---
+++ oops_packages.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class oops_packages {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); 
+// scanner ek class hai jmne sc naam ka ek object banaya hai new keyword k help se yeh cobstructor ek call ho raha hai  
+    int a = sc.nextInt();
+    System.out.println(a);
+    }
+}
+// grp of simililar types of classes, interfaces. and sub-packages
+// there are 2 types of packages:
+// 1. in buit package
+// 2. user defined package 
