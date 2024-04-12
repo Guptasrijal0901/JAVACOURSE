0863610ac4635bb28c19465aef7978b78e8df828
Srijal Gupta <guptasrijal0901@gmail.com>
2024-04-12 05:45:07 +0530

    lol

---
+++ oops_interfaces.java
@@ -0,0 +1,10 @@
+public class oops_interfaces {
+    public static void main(String[] args) {
+        
+    }
+}
+// class is blueprint of object 
+// interface is blue is bhule print of class
+// hierarcy in code can be done with the help of interfaces
+// car kaisi hogi vo batayenge fir maruti 800 kaisi hogi vo batayenge 
+// fir actuall maruti 800 banayenge 
