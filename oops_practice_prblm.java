4633fcf40ccf2cf90d3fcc26b948fb893d1d5336
Srijal Gupta <guptasrijal0901@gmail.com>
2024-04-13 12:26:14 +0530

    lol

---
+++ oops_practice_prblm.java
@@ -0,0 +1,10 @@
+public class oops_practice_prblm {
+    public static void main(String[] args) {
+        
+    }
+}
+// we cannot make private/protected class (unuseable)
+// private class hogi toh hme usee access hi nhi kar payenge 
+// properties private ho sakti hai lekin class nhi 
+// parent se child class ka object bana sakte hai 
+// lekin child se parent object nhi bana sakte 
