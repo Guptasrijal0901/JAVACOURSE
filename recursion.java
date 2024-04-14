423551cc32003fdc4d9fcddb9d07e58516fe3fb4
Srijal Gupta <guptasrijal0901@gmail.com>
2024-04-14 11:38:04 +0530

    lol

---
+++ recursion.java
@@ -0,0 +1,18 @@
+public class recursion {
+    public static void main(String[] args) {
+        
+    }
+}
+// we need to know about: 1. iteration 2. functions
+// used in mostly trees, graphs, DP 
+// recursive function are those function jo khud hi ko call karte hai choti problem k liye 
+// Defination: Recursion is a method of solving a computational problem where 
+// the solution depends on soltion to smaller instances of the same problem
+// base case: jo sabse chota problem jiska answer hme pata hota hai 
+// esme hm 2 direction mai jate hai 
+// pehle upar se niche jate hai towards base case 
+// then we go to upward direction problems solution ko combine karte karte 
+// step 1. base caes 
+// 2. hmahara kaam 
+// 3. inner function ko call karna 
+// it is same as princple of mathematical induction (PMI)
