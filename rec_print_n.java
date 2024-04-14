a943166366284432dcd13878101d272d97e58f7f
Srijal Gupta <guptasrijal0901@gmail.com>
2024-04-14 12:47:36 +0530

    lol

---
+++ rec_print_n.java
@@ -0,0 +1,19 @@
+public class rec_print_n {
+
+    public static void printDec(int n){
+    // yaha pe base class define kar diye 
+        if (n == 1) {
+            System.out.println(n);
+            return; // yaha pe 1 print hone k baad return kar jayenge -1 pe jayenge hi nhi 
+        }
+    // yaha par print karwa diye 
+        System.out.print(n + " ");
+    // fir yaha par function ko choota kar diye 
+        printDec(n-1);
+    }
+    public static void main(String[] args) {
+        int n = 10 ;
+        printDec(n);
+    }
+}
+// call stack : jaise jaise function call ho rahi vaise vaise stack banta hai usse call stack bolte hai 
