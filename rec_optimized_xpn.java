a79469a0715252aa46419fa8e33f2bef507171a0
Srijal Gupta <guptasrijal0901@gmail.com>
2024-04-24 10:05:16 +0530

    lol

--- rec_optimized_xpn.java
+++ rec_optimized_xpn.java
@@ -1,5 +1,23 @@
 public class rec_optimized_xpn {
+    public static int optimizedPower(int x , int n){
+        // base case 
+        if (n == 0) {
+            return 1;
+        }
+        if (n == 1) {
+            return x;
+        }
+        // for even 
+        int halfPowerSq = optimizedPower(x, n/2) * optimizedPower(x, n/2);
+        // for odd 
+        if (n % 2 != 0) {
+            halfPowerSq = x * halfPowerSq;
+        }
+        return halfPowerSq;
+    }
     public static void main(String[] args) {
-        
+        int x = 2;
+        int n = 10;
+        System.out.println(optimizedPower(x, n));
     }
 }
