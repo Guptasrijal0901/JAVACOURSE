a732c2453d6e1c0dfadb82c9dab5ae5e1afa3744
Srijal Gupta <guptasrijal0901@gmail.com>
2024-04-29 10:30:59 +0530

    lol

--- dnc_merg_sort.java
+++ dnc_merg_sort.java
@@ -1,6 +1,19 @@
 public class dnc_merg_sort {
-    public static 
+    public static void mergeSort(int arr[], int si, int ei){
+        if (si >= ei ) {
+            return;
+        }
+        // kaam
+        int mid = si (ei-si)/2;
+        mergeSort(arr, si, mid); // left part 
+        mergeSort(arr, mid+1, ei); // right part 
+        merge(arr, si, mid, ei);
+    }
+    public static void merge(int arr[], int si, int mid, int ei){
+        int temp[] new int [ei - si + 1];
+    }
     public static void main(String[] args) {
-        
+        int arr[] = {6, 3, 9, 5, 2, 8};
+        mergeSort(arr, 0, arr.length-1);
     }
 }
