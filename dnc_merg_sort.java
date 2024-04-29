76bdfee60558b7c74079226306b0041d959b4299
Srijal Gupta <guptasrijal0901@gmail.com>
2024-04-29 14:42:30 +0530

    lol

--- dnc_merg_sort.java
+++ dnc_merg_sort.java
@@ -2,7 +2,7 @@ public class dnc_merg_sort {
 
     public static void PrintArr(int arr[]){
         for(int i = 0 ; i < arr.length; i++){
-            System.out.println(arr[i] " ");
+            System.out.print(arr[i] + " ");
         }
         System.out.println();
     }
@@ -11,30 +11,50 @@ public class dnc_merg_sort {
             return;
         }
         // kaam
-        int mid = si + (ei-si)/2;
+        int mid = si + (ei-si)/2; // (si+ei)/2 se bhi chal jayega 
         mergeSort(arr, si, mid); // left part 
         mergeSort(arr, mid+1, ei); // right part 
         merge(arr, si, mid, ei);
     }
     public static void merge(int arr[], int si, int mid, int ei){
-        int temp[] = new int [ei - si + 1];
+        int temp[] = new int [ei - si + 1]; 
+        // 0 bhi hai naa toh usko bhi include karna hai esliye plus karna padega
         int i = si; // iterator for left part
         int j = mid+1; // iterator for right part
-        int k = 0 ; // iterator for tem arr
+        int k = 0 ; // iterator for tem array
         while(i <= mid && j <= ei){
-            if (arr[i]<arr[j]) {
+            if (arr[i]<arr[j]) { 
+                // aab hm elements ko compare kar rahe hai left array and right array
                 temp[k] = arr[i];
+                // agar kaam ho gya toh temp k andar store kar lena hai 
                 i++;
-                j++;
+                k++;
             }else{
                 temp[k] = arr[j];
                 j++ ;
                 k++;
             }
         }
+// first part k toh khtm ho gye aab 2nd part k array k bache elements ko conquer karna hai 
+        // left part mai agar kuch bacha hai toh 
+        while (i <= mid ) {
+            temp[k++] = arr[i++];
+        }
+        // right part 
+        while (j <= ei) {
+            temp[k++] = arr[j++];
+        }
+        // copy temp to original arr
+        for (k = 0, i = si; k <temp.length; k++, i++){
+            arr[i] = temp[k];
+        }
     }
     public static void main(String[] args) {
-        int arr[] = {6, 3, 9, 5, 2, 8};
-        System.out.println(mergeSort(arr, 0, arr.length-1));
+        int arr[] = {6, 3, 9, 5, 2, 8, -2};
+        mergeSort(arr, 0, arr.length-1);
+        PrintArr(arr);
     }
 }
+// TC: O(nlogn)
+// SC: O(n)
+// this technique is depth 
