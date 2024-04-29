ea44594e96b81e4724bbff0bac85eacb1718800e
Srijal Gupta <guptasrijal0901@gmail.com>
2024-04-29 14:06:20 +0530

    lol

--- dnc_merg_sort.java
+++ dnc_merg_sort.java
@@ -1,19 +1,40 @@
 public class dnc_merg_sort {
+
+    public static void PrintArr(int arr[]){
+        for(int i = 0 ; i < arr.length; i++){
+            System.out.println(arr[i] " ");
+        }
+        System.out.println();
+    }
     public static void mergeSort(int arr[], int si, int ei){
         if (si >= ei ) {
             return;
         }
         // kaam
-        int mid = si (ei-si)/2;
+        int mid = si + (ei-si)/2;
         mergeSort(arr, si, mid); // left part 
         mergeSort(arr, mid+1, ei); // right part 
         merge(arr, si, mid, ei);
     }
     public static void merge(int arr[], int si, int mid, int ei){
-        int temp[] new int [ei - si + 1];
+        int temp[] = new int [ei - si + 1];
+        int i = si; // iterator for left part
+        int j = mid+1; // iterator for right part
+        int k = 0 ; // iterator for tem arr
+        while(i <= mid && j <= ei){
+            if (arr[i]<arr[j]) {
+                temp[k] = arr[i];
+                i++;
+                j++;
+            }else{
+                temp[k] = arr[j];
+                j++ ;
+                k++;
+            }
+        }
     }
     public static void main(String[] args) {
         int arr[] = {6, 3, 9, 5, 2, 8};
-        mergeSort(arr, 0, arr.length-1);
+        System.out.println(mergeSort(arr, 0, arr.length-1));
     }
 }
