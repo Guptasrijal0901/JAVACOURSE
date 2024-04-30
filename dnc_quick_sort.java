122b8e77d89953c8789970117b036c7be610eb3e
Srijal Gupta <guptasrijal0901@gmail.com>
2024-04-30 12:55:11 +0530

    lol

--- dnc_quick_sort.java
+++ dnc_quick_sort.java
@@ -1,7 +1,7 @@
 public class dnc_quick_sort {
     public static void PrintArr(int arr[]){
         for(int i = 0 ; i<arr.length ; i++){
-            System.out.println(arr[i]+ " ");
+            System.out.print(arr[i]+ " ");
         }
         System.out.println();
     }
@@ -15,14 +15,28 @@ public class dnc_quick_sort {
     quicksort(arr, si, pIdx-1); //left
     quicksort(arr, pIdx, ei);
     }
-    public static void partition(int arr[], int si, int ei){
+    public static int partition(int arr[], int si, int ei){
         int pivot = arr[ei];
         int i = si-1; // to make place for elements smaller than pivot
-        for (int j =0; j<ei j++);
-           
+        for (int j = si; j<ei ; j++){
+           if (arr[j] <= pivot) {
+            i++;
+            // swap 
+            int temp = arr[j];
+            arr[j] = arr[i];
+            arr[i] = temp;
+        }
+        }
+        i++;
+        int temp = pivot;
+        arr[ei] = arr[i];
+        arr[i] = temp;
+        return i;
     }
     public static void main(String[] args) {
         int arr[] = {6, 3, 9, 8, 2, 5};
+        quicksort(arr, 0, arr.length-1);
+        PrintArr(arr);
     }
     
 }
