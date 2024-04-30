707af093191d75a81f6e27a36b8f110851f204e6
Srijal Gupta <guptasrijal0901@gmail.com>
2024-04-30 09:35:07 +0530

    lol

---
+++ dnc_quick_sort.java
@@ -0,0 +1,10 @@
+public class dnc_quick_sort {
+    public static void main(String[] args) {
+        
+    }
+    
+}
+//pivot and partition
+// pivot: jiske around rotate hoga (mostly last element)
+// partition: pivot number k ek traf smaller elements or ek taraf larger elements 
+// same fir recursively dono (left nd right)traf k elements ko quick sort karenge 
