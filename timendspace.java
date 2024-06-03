99ff5214c66ff281cb0a1e55edcd3fde43a8a3e7
Srijal Gupta <guptasrijal0901@gmail.com>
2024-06-03 21:10:22 +0530

    lol

--- timendspace.java
+++ timendspace.java
@@ -2,6 +2,6 @@ public class timendspace {
     
 }
 // amount of space or time taken up by an algorithm/code as function of input size 
-//NOT the actual time taken 
+// NOT the actual time taken 
 // -> experimental
 // -> theoretical  
