c90ce57b280151eec6e4479c6aff20406257b44a
Srijal Gupta <guptasrijal0901@gmail.com>
2024-07-16 17:08:02 +0530

    lol

--- backtrackfindsubsets.java
+++ backtrackfindsubsets.java
@@ -1,4 +1,12 @@
 public class backtrackfindsubsets {
+    public static void findSubsets(String str, String ans, int i) {
+        // base case
+        // recursion
+        // backtrack
+        // yes choice
+        findSubsets(str, ans + 1, i + 1);
+    }
+
     public static void main(String[] args) {
 
     }
