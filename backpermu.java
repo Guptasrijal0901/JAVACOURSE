3560eb7f71102d3d1ac6aee3aaf61b9a8fcecb3a
Srijal Gupta <guptasrijal0901@gmail.com>
2024-07-17 23:56:33 +0530

    lol

--- backpermu.java
+++ backpermu.java
@@ -1,6 +1,21 @@
 public class backpermu {
-    public static void main(String[] args) {
+    public static void permutation(String str, String ans) {
+        // base case
+        if (str.length() == 0) {
+            System.out.println(ans);
+            return;
+        }
+        // recursion - O(n*n!)
+        for (int i = 0; i < str.length(); i++) {
+            char curr = str.charAt(i);
+            String Newstr = str.substring(0, i) + str.substring(i + 1);
+            permutation(Newstr, ans + curr);
+        }
+    }
 
+    public static void main(String[] args) {
+        String str = "abc";
+        permutation(str, "");
     }
 
 }
