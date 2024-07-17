644bf50ba1ec21a6217abb97393d21ec336e6aad
Srijal Gupta <guptasrijal0901@gmail.com>
2024-07-17 22:18:07 +0530

    lol

---
+++ backpermu.java
@@ -0,0 +1,6 @@
+public class backpermu {
+    public static void main(String[] args) {
+
+    }
+
+}
--- backtrackfindsubsets.java
+++ backtrackfindsubsets.java
@@ -10,12 +10,21 @@ public class backtrackfindsubsets {
             }
             return;
         }
+        // This checks if the current index i is equal to the length of the string str,
+        // meaning we have considered all characters.
+        // If the subset ans is empty, it prints "null" (indicating an empty subset).
+        // Otherwise, it prints the current subset ans.
+        // Finally, it returns from the method.
         // recursion
         // backtrack
         // yes choice
         findSubsets(str, ans + str.charAt(i), i + 1);
         // no choice
         findSubsets(str, ans, i + 1);
+        // Yes choice: It includes the current character (str.charAt(i))
+        // in the subset and recursively calls findSubsets with the next index (i + 1).
+        // No choice: It excludes the current character and recursively calls
+        // findSubsets with the next index (i + 1).
     }
 
     public static void main(String[] args) {
@@ -24,3 +33,5 @@ public class backtrackfindsubsets {
     }
 }
 // backtracking steps
+// TC = O(n*2^n)
+// SC = O(n)
