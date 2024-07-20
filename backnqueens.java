5c6426d6061ac802b76820ebee5102f9ab110f08
Srijal Gupta <guptasrijal0901@gmail.com>
2024-07-20 23:36:54 +0530

    lol

--- backnqueens.java
+++ backnqueens.java
@@ -1,4 +1,24 @@
 public class backnqueens {
+    public static boolean isSafe(char board[][], int row, int col) {
+        // vertical up
+        for (int i = row - 1; i >= 0; i--) {
+            if (board[i][col] == 'Q') {
+                return false;
+            }
+        }
+        // diagonal left up
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q')
+                return false;
+        }
+        // diagonal right up
+        for (int i = row - 1, j = col + 1; i >= 0 && j < board.length; i--, j++) {
+            if (board[i][j] == 'Q')
+                return false;
+        }
+        return true;
+    }
+
     public static void nQueens(char board[][], int row) {
         // base case
         if (row == board.length) {
@@ -7,9 +27,12 @@ public class backnqueens {
         }
         // column loop
         for (int j = 0; j < board.length; j++) {
-            board[row][j] = 'Q';
-            nQueens(board, row + 1); // function call
-            board[row][j] = '.'; // backtracking step
+            if (isSafe(board, row, j)) {
+                board[row][j] = 'Q';
+                nQueens(board, row + 1); // function call
+                board[row][j] = 'X'; // backtracking step
+            }
+
         }
     }
 
@@ -24,12 +47,12 @@ public class backnqueens {
     }
 
     public static void main(String[] args) {
-        int n = 2;
+        int n = 4;
         char board[][] = new char[n][n];
         // initialize
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                board[i][j] = '.';
+                board[i][j] = 'X';
             }
         }
         nQueens(board, 0);
