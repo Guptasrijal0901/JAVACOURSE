fc99b9230ede8d15c9a3a1dff10a45a7cfcb67e5
Srijal Gupta <guptasrijal0901@gmail.com>
2024-07-27 15:07:06 +0530

    lol

--- backsudoku.java
+++ backsudoku.java
@@ -1,4 +1,8 @@
 public class backsudoku {
+    public static boolean sudokuSolver(int sudoku[][], int row, int col) {
+
+    }
+
     public static void main(String[] args) {
         int sudoku[][] = {
                 { 0, 0, 8, 0, 0, 0, 0, 0, 0 },
@@ -9,7 +13,6 @@ public class backsudoku {
                 { 9, 6, 0, 4, 0, 5, 3, 0, 0 },
                 { 0, 3, 0, 0, 7, 2, 0, 0, 4 },
                 { 0, 4, 9, 0, 3, 0, 0, 5, 7 },
-                { 8, 2, 7, 0, 0, 9, 0, 1, 3 }
-        };
+                { 8, 2, 7, 0, 0, 9, 0, 1, 3 } };
     }
 }
