17e749ebfeaba605138b2673e41303795ca666fc
Srijal Gupta <guptasrijal0901@gmail.com>
2024-07-28 23:20:51 +0530

    lol

--- backratinamaze.java
+++ backratinamaze.java
@@ -1,4 +1,9 @@
 public class backratinamaze {
+    public static void ratTrack(int maze[][], int n, int m) {
+        // base case
+        // recursion
+    }
+
     public static void main(String[] args) {
 
     }
