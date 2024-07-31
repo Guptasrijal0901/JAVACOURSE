86844843e6215aee3c57e62282610a6dfda9a576
Srijal Gupta <guptasrijal0901@gmail.com>
2024-07-31 18:08:53 +0530

    lol

--- arraylistintro.java
+++ arraylistintro.java
@@ -2,6 +2,14 @@ import java.util.*;
 
 public class arraylistintro {
     public static void main(String[] args) {
-        ArrayList<Integer> list = new ArrayList<>();
+        ArrayList<Integer> list1 = new ArrayList<>();
+        ArrayList<String> list2 = new ArrayList<>();
+        ArrayList<Boolean> list3 = new ArrayList<>();
+
+        list1.add(1);
+        list1.add(6);
+        list1.add(8);
+        // tc: O(1)
+        System.out.println(list1);
     }
 }
