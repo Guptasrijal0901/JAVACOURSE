df8b560381605ff6bf33542dd351ffbd7885a64a
Srijal Gupta <guptasrijal0901@gmail.com>
2024-08-02 15:49:09 +0530

    lol

---
+++ arraylistswap.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class arraylistswap {
+    public static void swap(int inx1, int inx2, ArrayList<Integer> list) {
+        int temp = list.get(inx1);
+        list.set(inx1, list.get(inx2));
+        list.set(inx2, temp);
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(4);
+        list.add(5);
+        list.add(8);
+        list.add(6);
+        list.add(2);
+        list.add(0);
+        System.out.println("Array: " + list);
+        swap(inx1, inx2, list);
+        System.out.println("Array afetr swaping" + list);
+
+    }
+
+}
