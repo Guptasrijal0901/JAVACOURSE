7ec33593fef6c9fd593778ab6739f13cb4b8efa4
Srijal Gupta <guptasrijal0901@gmail.com>
2024-08-06 22:55:25 +0530

    lol

--- arraylistmultiD.java
+++ arraylistmultiD.java
@@ -1,5 +1,28 @@
+import java.util.ArrayList;
+
 public class arraylistmultiD {
     public static void main(String[] args) {
+        ArrayList<ArrayList<Integer>> mainList = new ArrayList<>();
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(0);
+        list.add(1);
+        list.add(2);
+        mainList.add(list);
 
+        ArrayList<Integer> list2 = new ArrayList<>();
+        list2.add(3);
+        list2.add(4);
+        list2.add(5);
+        mainList.add(list2);
+
+        for (int i = 0; i < mainList.size(); i++) {
+            ArrayList<Integer> currlist = mainList.get(i);
+            for (int j = 0; j < currlist.size(); j++) {
+                System.out.print(currlist.get(j) + " ");
+            }
+            System.out.println();
+        }
+        System.out.println(mainList);
     }
+
 }
