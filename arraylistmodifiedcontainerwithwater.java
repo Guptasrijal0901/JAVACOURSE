ff548d4e26eeccd3452027c21a7d0140d305db82
Srijal Gupta <guptasrijal0901@gmail.com>
2024-08-11 00:51:56 +0530

    lol

--- arraylistmodifiedcontainerwithwater.java
+++ arraylistmodifiedcontainerwithwater.java
@@ -1,6 +1,26 @@
 import java.util.ArrayList;
 
 public class arraylistmodifiedcontainerwithwater {
+    public static int storeWater(ArrayList<Integer> height) {
+        int maxWater = 0;
+        int lp = 0;
+        int rp = height.size() - 1;
+        while (lp < rp) {
+            // calculate water area
+            int ht = Math.min(height.get(lp), height.get(rp));
+            int wt = rp - lp;
+            int currWater = ht * wt;
+            maxWater = Math.max(maxWater, currWater);
+            // update pointer
+            if (height.get(lp) < height.get(rp)) {
+                lp++;
+            } else {
+                rp--;
+            }
+        }
+        return maxWater;
+    }
+
     public static void main(String[] args) {
         ArrayList<Integer> height = new ArrayList<>();
         height.add(1);
@@ -13,5 +33,9 @@ public class arraylistmodifiedcontainerwithwater {
         height.add(3);
         height.add(7);
         System.out.println("Heights: " + height);
+        System.out.println("Maximum stored water: " + storeWater(height));
     }
 }
+
+// 2 pointer approach to solve this
+// kam height wale ko shift karenge
