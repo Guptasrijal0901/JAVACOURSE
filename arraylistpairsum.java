7a7fd61395aa00815719fdc8c85f5fff1a5a7c0a
Srijal Gupta <guptasrijal0901@gmail.com>
2024-08-13 00:14:04 +0530

    lol

--- arraylistpairsum.java
+++ arraylistpairsum.java
@@ -1,8 +1,23 @@
 import java.util.ArrayList;
 
 public class arraylistpairsum {
+    // Brute force
+    public static boolean pairSum(ArrayList<Integer> list, int target) {
+        for (int i = 0; i < list.size(); i++) {
+            for (int j = i + 1; j < list.size(); j++) {
+                if (list.get(i) + list.get(j) == target) {
+                    System.out.println("Pair found: (" + list.get(i) + ", " + list.get(j) + ")");
+                    return true;
+
+                }
+            }
+        }
+        return false;
+    }
+
+    // optimized approach to get less TC by using
     public static void main(String[] args) {
-        // brute force
+
         ArrayList<Integer> list = new ArrayList<>();
         list.add(1);
         list.add(2);
@@ -11,5 +26,6 @@ public class arraylistpairsum {
         list.add(5);
         list.add(6);
         System.out.println(list);
+        System.out.println(pairSum(list, 5));
     }
 }
