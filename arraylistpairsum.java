fa465aec2d3d25c8efbdea49b55951767f59ebc4
Srijal Gupta <guptasrijal0901@gmail.com>
2024-08-13 00:03:44 +0530

    lol

--- arraylistpairsum.java
+++ arraylistpairsum.java
@@ -1,5 +1,15 @@
+import java.util.ArrayList;
+
 public class arraylistpairsum {
     public static void main(String[] args) {
-
+        // brute force
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        System.out.println(list);
     }
 }
