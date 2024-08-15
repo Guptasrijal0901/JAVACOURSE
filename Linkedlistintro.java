00b29c6d409733b224de20ea2c4d17a0984552fc
Srijal Gupta <guptasrijal0901@gmail.com>
2024-08-15 15:46:39 +0530

    lol

--- Linkedlistintro.java
+++ Linkedlistintro.java
@@ -17,7 +17,7 @@ public class Linkedlistintro {
 
     public void addFirst(int data) {
         // -create new node
-        // empty ll
+        // empty linkedlist
         Node newNode = new Node(data);
         if (head == null) {
             head = tail = newNode;
@@ -31,13 +31,67 @@ public class Linkedlistintro {
     }
 
     // remove
+    // -create new node
+    // -tail.next = newNode
+    // -tail = newNode
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        tail.next = newNode;
+        tail = newNode;
+    }
+
     // print
+    public void print() { // TC: O(n)
+        if (head == null) {
+            System.out.println("LL is empty.");
+            return;
+        }
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + "-> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    // add in middle
+    public void addInmiddle(int idx, int data) {
+        if (idx == 0) { // jab first mai add karna ho
+            addFirst(data);
+            return;
+        }
+        Node newNode = new Node(data);
+        Node temp = head;
+        int i = 0;
+        while (i < idx - 1) {
+            temp = temp.next;
+            i++;
+        }
+        // i = idx-1; temp -> prev
+        newNode.next = temp.next;
+        temp.next = newNode;
+    }
+
     // search
     public static void main(String[] args) {
         Linkedlistintro ll = new Linkedlistintro();
-        ll.addFirst(1);
+        ll.print();
         ll.addFirst(2);
+        ll.print();
+        ll.addFirst(1);
+        ll.print();
+        ll.addLast(3);
+        ll.print();
+        ll.addLast(4);
+        ll.print();
+        ll.addInmiddle(2, 9);
+        ll.print();
     }
 }
 // it is class of nodes which have object
-// TC: O(1)
+// TC: O(1) of both addlast and addfirst
+// TC: O(1) of add node in the middle
