1aab2169e9cb50d245bf24ec3d4d81e761ea205c
Srijal Gupta <guptasrijal0901@gmail.com>
2024-08-15 13:24:34 +0530

    lol

--- Linkedlistintro.java
+++ Linkedlistintro.java
@@ -13,16 +13,31 @@ public class Linkedlistintro {
     public static Node tail;
 
     // methods to
-    // add
+    // add: we can add at first or last
+
+    public void addFirst(int data) {
+        // -create new node
+        // empty ll
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+
+        // -new node ko head banana
+        newNode.next = head; // linking step
+        // -head = new node
+        head = newNode;
+    }
+
     // remove
     // print
     // search
     public static void main(String[] args) {
         Linkedlistintro ll = new Linkedlistintro();
-        ll.head = new Node(1);
-        ll.head.next = new Node(2);
-
+        ll.addFirst(1);
+        ll.addFirst(2);
     }
 }
 // it is class of nodes which have object
-//
+// TC: O(1)
