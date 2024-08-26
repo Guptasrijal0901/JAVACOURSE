8f3a096d80ec645c3daa42f3452844bbdaebda84
Srijal Gupta <guptasrijal0901@gmail.com>
2024-08-26 12:34:31 +0530

    lol

--- linkedlist2.java
+++ linkedlist2.java
@@ -34,13 +34,43 @@ public class linkedlist2 {
     // slow ko head bana do
     // slow +1 and fast +1
     // null banana
+    public static void removeCycle() {
+        // detect cycle
+        Node slow = head;
+        Node fast = head;
+        boolean cycle = false;
+        while (fast != null && fast.next != null) {
+            slow = slow.next; // +1
+            fast = fast.next.next; // +2
+            if (slow == fast) {
+                cycle = true;
+                break;
+            }
+        }
+        if (cycle = false) {
+            return;
+        }
+        // find meeting point
+        slow = head;
+        Node prev = null; // last node
+        while (slow != fast) {
+            prev = fast;
+            slow = slow.next;
+            fast = fast.next;
+        }
+        // remove cycle -> last.next = null
+        prev.next = null;
+    }
 
     public static void main(String[] args) {
         head = new Node(1);
-        head.next = new Node(2);
+        Node temp = new Node(2);
+        head.next = temp;
         head.next.next = new Node(3);
-        head.next.next.next = head;
-        // 1-> 2 -> 3 -> 1
+        head.next.next.next = temp;
+        // 1-> 2 -> 3 -> 2
+        System.out.println(isCycle());
+        removeCycle();
         System.out.println(isCycle());
     }
 }
