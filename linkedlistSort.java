7693071c76835b34ae2ef2cf4cd59b7a297c5bb2
Srijal Gupta <guptasrijal0901@gmail.com>
2024-08-30 15:18:10 +0530

    lol

--- linkedlistSort.java
+++ linkedlistSort.java
@@ -3,3 +3,9 @@ public class linkedlistSort {
 
     }
 }
+// TC: nlogn
+// 1. ll ka middle node find karna hai
+// 2. divide ll in 2 parts: left half and right half-> next node ko null banana
+// hai-> or dono k liye merge sort k liye call lagana hai
+// 3. call merge to combine both of them
+// to find miiddle node-> slow fast pointer approach
