57c80e1d581e96b97a54c2512f302e13edd7110b
Srijal Gupta <guptasrijal0901@gmail.com>
2024-09-04 23:21:39 +0530

    lol

--- linkedlistDoubly.java
+++ linkedlistDoubly.java
@@ -1,33 +1,59 @@
+import java.util.*;
+
 public class linkedlistDoubly {
-    public class Node(
+    public class Node {
         int data;
         Node next;
         Node prev;
-// constructor
-        public Node(int data){
+
+        // constructor
+        public Node(int data) {
             this.data = data;
             this.next = null;
             this.prev = null;
         }
-    )
-    public static Node;
+    }
+
+    public static Node head;
     public static Node tail;
     public static int size;
 
     // add
-    public void addFirst(int data){
+    public void addFirst(int data) {
         Node newNode = new Node(data);
+        size++;
         if (head == null) {
-            
+            head = tail = newNode;
+            return;
         }
+        newNode.next = head;
+        head.prev = newNode;
+        head = newNode;
     }
-    // remove
-    //
 
-    public static void main(String[] args) {
-        
+    // print
+    public void print() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " <-> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
     }
-    
+
+    // remove
+    public static void main(String[] args) {
+        linkedlistDoubly dll = new linkedlistDoubly();
+        dll.addFirst(6);
+        dll.addFirst(5);
+        dll.addFirst(4);
+        dll.addFirst(3);
+        dll.addFirst(2);
+        dll.addFirst(1);
+        dll.print();
+        System.out.println(dll.size);
+    }
+
 }
 // A doubly linked list in Java is a data structure where each element (called a
 // node) contains three parts:
