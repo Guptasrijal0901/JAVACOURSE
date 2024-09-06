d28e849ed27097ab9866b7dce8c081aa3654d1a6
Srijal Gupta <guptasrijal0901@gmail.com>
2024-09-06 16:00:18 +0530

    lol

---
+++ stackFromLinkedlist.java
@@ -0,0 +1,5 @@
+public class stackFromLinkedlist {
+    public static void main(String[] args) {
+
+    }
+}
--- stackIntro.java
+++ stackIntro.java
@@ -1,11 +1,48 @@
-public class stackIntro {
-    public static void main(String[] args) {
+import java.util.*;
 
+public class stackIntro {
+    static class stackFromArray {
+        static ArrayList<Integer> list = new ArrayList<>();
+
+        // ISEMPTY
+        public static boolean isEmpty() {
+            return list.size() == 0;
+        }
+
+        // PUSH
+        public static void push(int data) {
+            list.add(data);
+        }
+
+        // POP
+        public static int pop() {
+            if (isEmpty()) {
+                return -1;
+            }
+            int top = list.get(list.size() - 1);
+            list.remove(list.size() - 1);
+            return top;
+        }
+
+        // PEEK
+        public static int peek() {
+            if (isEmpty()) {
+                return -1;
+            }
+            return list.get(list.size() - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        Stack s = new Stack();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.push(4);
+        s.push(5);
+        while (!s.isEmpty()) {
+            System.out.println(s.peek());
+            s.pop();
+        }
     }
 }
-// implicit stack:jab memory hmhare liye banati hai jaise backtracking mai
-// explicit stack: jab coders stack bante hai
-// LIFO
-// implementation from array(fixed size), arraylist(variable size, implement
-// from top), linkedlist(variable size, implement from head)
-// most efficient is array list and linkedlist
