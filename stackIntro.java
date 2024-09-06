d4385200502d10af72442af6391d13e5e9be3c3d
Srijal Gupta <guptasrijal0901@gmail.com>
2024-09-06 09:58:39 +0530

    lol

--- stackIntro.java
+++ stackIntro.java
@@ -3,3 +3,9 @@ public class stackIntro {
 
     }
 }
+// implicit stack:jab memory hmhare liye banati hai jaise backtracking mai
+// explicit stack: jab coders stack bante hai
+// LIFO
+// implementation from array(fixed size), arraylist(variable size, implement
+// from top), linkedlist(variable size, implement from head)
+// most efficient is array list and linkedlist
