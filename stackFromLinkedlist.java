a143ce82950496c4b8d27a75afb47fa4679ec84d
Srijal Gupta <guptasrijal0901@gmail.com>
2024-09-06 17:05:04 +0530

    lol

--- stackFromLinkedlist.java
+++ stackFromLinkedlist.java
@@ -1,5 +1,67 @@
 public class stackFromLinkedlist {
-    public static void main(String[] args) {
+    static class Node {
+        int data;
+        Node next;
 
+        // constructor to pass data
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    static class Stack {
+        // create head node
+        static Node head = null;
+
+        public static boolean isEmpty() {
+            return head == null;
+        }
+
+        // PUSH
+        public static void push(int data) {
+            // push means to replace head with new node
+            Node newNode = new Node(data);
+            if (isEmpty()) {
+                head = newNode;
+                return;
+            }
+
+            newNode.next = head;
+            head = newNode;
+        }
+
+        // POP
+        public static int pop() {
+            if (isEmpty()) {
+                return -1;
+            }
+            int top = head.data;
+            head = head.next;
+            return top;
+        }
+
+        // PEEK
+        public static int peek() {
+            if (isEmpty()) {
+                return -1;
+            }
+            return head.data;
+        }
+    }
+
+    public static void main(String[] args) {
+        Stack s = new Stack();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.push(4);
+        s.push(5);
+        // bas upar se nikal dega
+        // s.pop();
+        while (!s.isEmpty()) {
+            System.out.println(s.peek());
+            s.pop();
+        }
     }
 }
