57ade9a570af06e8f580a0c1d8e3317a5875c5a5
Srijal Gupta <guptasrijal0901@gmail.com>
2024-09-07 16:06:18 +0530

    lol

--- stackQ1.java
+++ stackQ1.java
@@ -1,5 +1,28 @@
-public class stackQ1 {
-    public static void main(String[] args) {
+import java.util.Stack;
 
+public class stackQ1 {
+    public static void pushAtBottom(Stack<Integer> s, int data) {
+        if (s.isEmpty()) {
+            s.push(data);
+            return;
+        }
+        int top = s.pop();
+        pushAtBottom(s, data);
+        s.push(top);
+    }
+
+    public static void main(String[] args) {
+        Stack<Integer> s = new Stack<>();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+
+        pushAtBottom(s, 4);
+        while (!s.isEmpty()) {
+            System.out.println(s.pop());
+        }
     }
 }
+// push at the bottom of the stack
+// amazon mai aaya hai
+// recursion mai jo cheez phele nikalti hai vo last mai wapas aati hai
