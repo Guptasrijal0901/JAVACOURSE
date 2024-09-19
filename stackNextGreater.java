9db3d7c673a0e0c31c49dd4860b0a4f0f74a6768
Srijal Gupta <guptasrijal0901@gmail.com>
2024-09-19 22:50:38 +0530

    lol

--- stackNextGreater.java
+++ stackNextGreater.java
@@ -5,7 +5,3 @@ public class stackNextGreater {
 }
 // the next greater element of some element x in an array is the first greater
 // element that is to right of x in the smane array
-// its tough and required more time to understand
-// one more day
-// one more day no study
-// aaj toh bhut ganda hua
