1ebc3a8fec0e23d67bf87b447909e87c406301d1
Srijal Gupta <guptasrijal0901@gmail.com>
2024-09-23 00:04:41 +0530

    lol

--- stackDuplicatePara.java
+++ stackDuplicatePara.java
@@ -1,6 +1,11 @@
 public class stackDuplicatePara {
-    public static void main(String[] args) {
+    public static boolean isDuplicate(String str )
 
+    public static void main(String[] args) {
+        String str = "((a+b))";
+        String str2 = " (a+b)";
+        System.out.println(isValid(str));
+        System.out.println(isValid(str2));
     }
 }
 // TC: O(n)
