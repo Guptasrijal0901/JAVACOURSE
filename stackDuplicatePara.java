31c58d9865272106e9980cb9cc91cd5ad15bf0e0
Srijal Gupta <guptasrijal0901@gmail.com>
2024-09-24 23:21:38 +0530

    lol

--- stackDuplicatePara.java
+++ stackDuplicatePara.java
@@ -1,11 +1,38 @@
+import java.util.*;
+
 public class stackDuplicatePara {
-    public static boolean isDuplicate(String str )
+    public static boolean isDuplicate(String str) {
+        Stack<Character> s = new Stack<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            // closing
+            if (ch == ')') {
+                int count = 0;
+                while (s.peek() != '(') {
+                    s.pop();
+                    count++;
+                }
+                if (count < 1) {
+                    return true; // duplicate
+                } else {
+                    s.pop(); // opening pair
+                }
+            } else {
+                // opening
+                s.push(ch);
+            }
+        }
+        return false;
+    }
 
     public static void main(String[] args) {
         String str = "((a+b))";
         String str2 = " (a+b)";
-        System.out.println(isValid(str));
-        System.out.println(isValid(str2));
+        System.out.println(isDuplicate(str));
+        System.out.println(isDuplicate(str2));
     }
 }
 // TC: O(n)
+// 24 september
