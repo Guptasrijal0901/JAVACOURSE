f48f18a9f4e87309a4e42de8ba4e46f89e6716eb
Srijal Gupta <guptasrijal0901@gmail.com>
2024-09-26 21:44:54 +0530

    lol

--- queueIntro.java
+++ queueIntro.java
@@ -4,3 +4,15 @@ public class queueIntro {
     }
 
 }
+// A Queue in Java is a data structure that follows the First In, First Out
+// (FIFO) principle.
+// This means that the first element added to the queue will be the first one to
+// be removed.
+
+// You can think of a queue like a line of people waiting for something—like in
+// a bank or at a movie theater.
+// The person who gets in line first is the first one to be served, and the last
+// person in line has to wait until everyone before them is served.
+// add O(n)- Enque
+// remove O(1)- Dequeue
+// peek O(1)- Front
