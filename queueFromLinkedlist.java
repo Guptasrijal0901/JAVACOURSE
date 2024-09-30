46fba40c239ba4ed0edfa0e3df990b5a5e2e8ec7
Srijal Gupta <guptasrijal0901@gmail.com>
2024-09-30 15:55:38 +0530

    lol

--- queueFromLinkedlist.java
+++ queueFromLinkedlist.java
@@ -4,8 +4,19 @@ public class queueFromLinkedlist {
         Node next;
 
         Node(int data) {
-
+            this.data = data;
+            this.next = null;
         }
+    }
+
+    static class Queue {
+        static Node head = null;
+        static Node tail = null;
+
+    }
+
+    public static boolean isEmpty() {
+        return head == null && tail == null;
     }
 
     public static void main(String[] args) {
