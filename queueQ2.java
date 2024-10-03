c930fe0736b03535cd97b3066bfc309db659b389
Srijal Gupta <guptasrijal0901@gmail.com>
2024-10-03 22:47:51 +0530

    lol

--- queueQ2.java
+++ queueQ2.java
@@ -24,6 +24,8 @@ public class queueQ2 {
         }
 
         // remove
+
+        
         public static int remove() {
             if (isEmpty()) {
                 System.out.println("Queue is empty");
