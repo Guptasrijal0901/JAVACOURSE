3384a63c1ae7b379216dd776ba3a8e519664f4af
Srijal Gupta <guptasrijal0901@gmail.com>
2024-10-07 22:55:04 +0530

    lol

--- queueInterleave.java
+++ queueInterleave.java
@@ -1,6 +1,24 @@
-public class queueInterleave {
-    public static void main(String[] args) {
+import java.util.*;
 
+public class queueInterleave {
+    public static void InterLeave(Queue<Integer> q){
+        Queue<Integer> 
+    }
+
+    public static void main(String[] args) {
+        Queue<Integer> q = new Linkedlist<>();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.add(5);
+        q.add(6);
+        q.add(7);
+        q.add(8);
+        q.add(9);
+        q.add(10);
+        q.add(11);
+        q.add(12);
     }
 }
 // TC: O(n)
