82aee8a5a45ee063d3946ac01f835ec26dc1e9dd
Srijal Gupta <guptasrijal0901@gmail.com>
2024-10-27 10:27:57 +0530

    lol

--- queueDequeue.java
+++ queueDequeue.java
@@ -1,8 +1,5 @@
 public class queueDequeue {
     public static void main(String[] args) {
-        System.out.println("hello");
-        System.out.println("hello");
 
-        System.out.println("hello");
     }
 }
