98a666af91331eb67305ba11353b25ca47e66768
Srijal Gupta <guptasrijal0901@gmail.com>
2024-10-28 15:55:34 +0530

    lol

--- queueFromdeque.java
+++ queueFromdeque.java
@@ -1,4 +1,23 @@
+import java.util.Deque;
+import java.util.LinkedList;
+
 public class queueFromdeque {
+    static class stack {
+        Deque<Integer> deque = new LinkedList<>();
+
+        public void push(int data) {
+            dequeue.addLast(data);
+        }
+
+        public int pop() {
+            return deque.removeLast();
+        }
+
+        public int peek() {
+            return deque.getLast();
+        }
+    }
+
     public static void main(String[] args) {
 
     }
