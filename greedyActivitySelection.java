55baf7dc091e41d29ad1e941e494bb8e6475bb04
Srijal Gupta <guptasrijal0901@gmail.com>
2024-11-06 20:45:37 +0530

    lol

---
+++ greedyActivitySelection.java
@@ -0,0 +1,5 @@
+public class greedyActivitySelection {
+    public static void main(String[] args) {
+
+    }
+}
--- greedyIntro.java
+++ greedyIntro.java
@@ -3,3 +3,8 @@ public class greedyIntro {
 
     }
 }
+// Greedy algorithms is the problem solving techniques whwew we make the locally
+// optimum choice at each stage and hope to achieve a global optimum.
+// Pros: simple and easy
+// Good enough TC
+// Cons: A lot of time, global optimum is not achieved
