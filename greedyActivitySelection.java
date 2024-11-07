2c9739b7ee86f724a31e67c85b776e888bc47d08
Srijal Gupta <guptasrijal0901@gmail.com>
2024-11-07 19:09:38 +0530

    lol

--- greedyActivitySelection.java
+++ greedyActivitySelection.java
@@ -3,3 +3,7 @@ public class greedyActivitySelection {
 
     }
 }
+// You are given n activities with their start and end times.
+// Select the maximum number of activities that can be perform by a single
+// person, assuming that a person can only work on a single activity at a time.
+// Activities are sorted according to end time
