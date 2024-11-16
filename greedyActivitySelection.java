155a914616ddd6164609a3072859a3e2e1cb3206
Srijal Gupta <guptasrijal0901@gmail.com>
2024-11-16 20:10:25 +0530

    lol

--- greedyActivitySelection.java
+++ greedyActivitySelection.java
@@ -23,7 +23,7 @@ public class greedyActivitySelection {
         }
         System.out.println("max activities: " + maxAct);
         for (int i = 0; i < ans.size(); i++) {
-            System.out.println("A" + ans.get(i));
+            System.out.print("A" + ans.get(i) + " ");
         }
     }
 }
