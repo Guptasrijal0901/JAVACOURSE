2a959eba41c8a4d314a096ad3f0ad3338c9757fb
Srijal Gupta <guptasrijal0901@gmail.com>
2024-11-30 20:47:47 +0530

    lol

--- greedyFractionalKnapsack.java
+++ greedyFractionalKnapsack.java
@@ -4,3 +4,9 @@ public class greedyFractionalKnapsack {
     }
 
 }
+// given the weight and values of N items, put these in a knapsack of capacity W
+// to get the maximum total value in the knapsack
+// value= [60, 100, 120]
+// weight= [10, 20, 30]
+// W= 240
+// Approach: weight kam hona chaiye or value zayada -> ratio: value/weight
