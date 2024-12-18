c3c5f2fb284d63593abb1aa4414c2f02d3756218
Srijal Gupta <guptasrijal0901@gmail.com>
2024-12-18 19:16:14 +0530

    lol

--- greedyMaxLengthChainPair.java
+++ greedyMaxLengthChainPair.java
@@ -1,5 +1,9 @@
 public class greedyMaxLengthChainPair {
     public static void main(String[] args) {
-        
+
     }
 }
+// You are given n pairs of number. In every pair.
+// The first number is always smaller than the second number.
+// A pair (c,d) can come afetr pair (a, b) if b,c.
+// Find the longest chain which can be formed from a given set of pairs.
