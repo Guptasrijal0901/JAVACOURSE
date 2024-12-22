338659e75962ce9dfef2a6d0d5ef8f0f3c86a240
Srijal Gupta <guptasrijal0901@gmail.com>
2024-12-22 13:37:33 +0530

    lol

--- greedyIndianCoins.java
+++ greedyIndianCoins.java
@@ -4,3 +4,10 @@ public class greedyIndianCoins {
     }
 
 }
+// we are given an infinite supply of denominations[1, 2, 5, 20, 50, 100, 500,
+// 2000]
+// find minimum no. of coins to make change for a value V.
+// this is only for this coins system
+// APPROACH
+// MAXIMUM POSSIBLE COIN DENGE
+// SORT IN DESCENDING
