64327dec8d44523c2306b55f5e13607f017e1505
Srijal Gupta <guptasrijal0901@gmail.com>
2024-12-25 00:24:47 +0530

    lol

--- greedyJobSequencing.java
+++ greedyJobSequencing.java
@@ -3,3 +3,10 @@ public class greedyJobSequencing {
 
     }
 }
+// Given an array of jobs where every job has a deadline and profit if job is
+// finished before the deadline.
+// It is also given that every job takes a single unit of time, so the ninimum
+// possible deadline for any job is 1.
+// Maximize the total profit if only one job can be scheduled at a time.
+
+// jiske basis pe maximum ya minimum karna uske basis pe sorting kar do
