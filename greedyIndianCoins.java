8f6c8a27b2e573b377cf668ff85ff9b070d4e5cd
Srijal Gupta <guptasrijal0901@gmail.com>
2024-12-24 23:07:01 +0530

    lol

--- greedyIndianCoins.java
+++ greedyIndianCoins.java
@@ -1,6 +1,31 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class greedyIndianCoins {
     public static void main(String[] args) {
+        Integer coins[] = { 1, 2, 5, 10, 20, 50, 100, 500, 2000 };
+        Arrays.sort(coins, Comparator.reverseOrder());
+        int countOfCoins = 0;
+        int ammout = 590;
+        ArrayList<Integer> ans = new ArrayList<>();
 
+        for (int i = 0; i < coins.length; i++) {
+            if (coins[i] <= ammout) {
+                while (coins[i] <= ammout) {
+                    countOfCoins++;
+                    ans.add(coins[i]);
+                    ammout = ammout - coins[i];
+                }
+            }
+        }
+        System.out.println("Total(min) coins used = " + countOfCoins);
+
+        for (int i = 0; i < ans.size(); i++) {
+            System.out.print(ans.get(i) + " ");
+        }
+        System.out.println();
     }
 
 }
