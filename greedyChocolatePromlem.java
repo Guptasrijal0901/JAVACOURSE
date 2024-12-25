01da60b6dfbb5253d9c6d0a86782a1d6858f4142
Srijal Gupta <guptasrijal0901@gmail.com>
2024-12-25 13:12:38 +0530

    25

---
+++ greedyChocolatePromlem.java
@@ -0,0 +1,13 @@
+public class greedyChocolatePromlem {
+    public static void main(String[] args) {
+
+    }
+
+}
+// We are given a bar of chocolate composed of mXn square pieces. One should
+// break the chocolate into single square.
+// Each break of a part of the chocolate is charged is a cost expresed by a
+// positive integer.
+// this cost does not depend on the size of the part that is being broken but
+// only depends on the line the break goes along.
+// Let
--- greedyJobSequencing.java
+++ greedyJobSequencing.java
@@ -1,4 +1,7 @@
+import java.util.*;
+
 public class greedyJobSequencing {
+
     public static void main(String[] args) {
 
     }
