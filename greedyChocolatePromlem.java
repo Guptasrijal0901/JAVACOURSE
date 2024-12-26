502756223157f264454e738a01a5dc780bed9062
Srijal Gupta <guptasrijal0901@gmail.com>
2024-12-26 23:32:36 +0530

    lol

--- greedyChocolatePromlem.java
+++ greedyChocolatePromlem.java
@@ -10,4 +10,7 @@ public class greedyChocolatePromlem {
 // positive integer.
 // this cost does not depend on the size of the part that is being broken but
 // only depends on the line the break goes along.
-// Let
+// Let us donate the costs of breaking along consecutive vertical lines with x1,
+// x2, ......., xm-1
+// and along horzontal lines with y1, y2, ......yn-1
+// compute the minimal cost of breaking the whole chocolate inti single squares
