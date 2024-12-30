274a180bd2896f890d358e24afecad963b6a8fee
Srijal Gupta <guptasrijal0901@gmail.com>
2024-12-30 20:15:31 +0530

    30

--- buildTree.java
+++ buildTree.java
@@ -1,5 +1,26 @@
 public class buildTree {
-    public static void main(String[] args) {
+    static class Node {
+        int data;
+        Node leftNode;
+        Node rightNode;
 
+        Node(int data) {
+            this.data = data;
+            this.leftNode = null;
+            this.rightNode = null;
+        }
+
+    }
+
+    static class BinaryTree {
+        static int idx = -1;
+
+        public static Node buildTree(int nodes[]) {
+
+        }
+    }
+
+    public static void main(String[] args) {
+        int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
     }
 }
