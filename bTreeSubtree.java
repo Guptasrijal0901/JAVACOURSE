c9ed90807cc7f06f03ab3d3ebc17d978826a0036
Srijal Gupta <guptasrijal0901@gmail.com>
2025-01-12 23:06:46 +0530

    12

--- bTreeSubtree.java
+++ bTreeSubtree.java
@@ -5,4 +5,7 @@ public class bTreeSubtree {
 }
 // given the roots of two bimary tree root and subroot,
 // return true if there is a subtree of root with the same structure and node
-// values of subroot anf false otherwise
+// values of subroot anf false otherwise.
+// APPROACH
+// 1. FIND SUBROOT IN TREE
+// 2. CHECK IDENTICAL(SUBROOT, NODE SUBTREE)
