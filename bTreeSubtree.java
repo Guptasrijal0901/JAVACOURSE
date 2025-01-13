7c7149b841a992fda0e51cd0c47a77486003c24a
Srijal Gupta <guptasrijal0901@gmail.com>
2025-01-13 15:33:37 +0530

    13

--- bTreeSubtree.java
+++ bTreeSubtree.java
@@ -9,3 +9,8 @@ public class bTreeSubtree {
 // APPROACH
 // 1. FIND SUBROOT IN TREE
 // 2. CHECK IDENTICAL(SUBROOT, NODE SUBTREE)
+// NON IDENTICAL CONDITIONS:
+// 1. node.data!= subroot.data
+// 2. node=null || subroot = nul
+// 3. leftsubtree -> non identical
+// 4. rightsubtree -> non identical
