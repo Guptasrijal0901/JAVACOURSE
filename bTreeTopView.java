c7bc0f363fed825da929c9626e3e4b7b81d2a1ae
Srijal Gupta <guptasrijal0901@gmail.com>
2025-01-14 13:02:10 +0530

    14

---
+++ bTreeTopView.java
@@ -0,0 +1,20 @@
+import org.w3c.dom.Node;
+
+public class bTreeTopView {
+    static class Node {
+        int data;
+        Node left, right;
+
+        public Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
