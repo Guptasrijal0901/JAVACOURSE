f549ae6bec4b83a98edbe7d9a8063d3c9c952c6a
Srijal Gupta <guptasrijal0901@gmail.com>
2025-01-23 15:18:48 +0530

    23

--- bTreeLCA.java
+++ bTreeLCA.java
@@ -1,6 +1,20 @@
+import org.w3c.dom.Node;
+
 public class bTreeLCA {
     public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        int n1 = 4, n2 = 5;
 
     }
 
 }
+// APROACH: 
+// 1. root se number tk path calculate karna h
+// 2. loop se last common node nikal lo 
