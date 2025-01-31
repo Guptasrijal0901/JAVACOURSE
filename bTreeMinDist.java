a00c6a8b2210b83c0e28ac7b9fcfb8f215518e6a
Srijal Gupta <guptasrijal0901@gmail.com>
2025-01-31 21:16:31 +0530

    31

--- bTreeMinDist.java
+++ bTreeMinDist.java
@@ -1,5 +1,20 @@
+import org.w3c.dom.Node;
+
 public class bTreeMinDist {
+    public class bTreeLCA2 {
+    static class Node {
+        int data;
+        Node left, right;
+
+        public Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
     public static void main(String[] args) {
 
     }
 }
+// best approach to find this to find the LCA of that number each and find the
+// distance then sum both of that
