92c2d6dcc234b6b773c93ca36a44aeb454a74d67
Srijal Gupta <guptasrijal0901@gmail.com>
2025-02-11 15:15:29 +0530

    09

--- bTreeMinDist.java
+++ bTreeMinDist.java
@@ -1,7 +1,6 @@
 import org.w3c.dom.Node;
 
 public class bTreeMinDist {
-    public class bTreeLCA2 {
     static class Node {
         int data;
         Node left, right;
@@ -12,6 +11,7 @@ public class bTreeMinDist {
             this.right = null;
         }
     }
+
     public static Node lca2(Node root, int n1, int n2) {
         // leftLCA = null and rightLCA = null
         if (root == null || root.data == n1 || root.data == n2) {
@@ -32,6 +32,35 @@ public class bTreeMinDist {
         // dono taraf ek ek value hai -> left mai bhi or right mai bhi
         return root;
     }
+
+    public static int lcaDist(Node root, int n) {
+        if (root == null) {
+            return -1;
+        }
+
+        if (root.data == n) {
+            return 0;
+        }
+        int leftDist = lcaDist(root.left, n);
+        int rightDist = lcaDist(root.right, n);
+
+        if (leftDist == -1 && rightDist == -1) {
+            return -1;
+        } else if (leftDist == -1) {
+            return rightDist + 1;
+        } else {
+            return leftDist + 1;
+        }
+    }
+
+    public static int minDistance(Node root, int n1, int n2) {
+        Node lca = lca2(root, n1, n2);
+        int dist1 = lcaDist(lca, n1);
+        int dist2 = lcaDist(lca, n2);
+
+        return dist1 + dist2;
+    }
+
     public static void main(String[] args) {
         Node root = new Node(1);
         root.left = new Node(2);
@@ -42,6 +71,8 @@ public class bTreeMinDist {
         root.right.right = new Node(7);
 
         int n1 = 4, n2 = 6;
+
+        System.out.println(minDistance(root, n1, n2));
     }
 }
 // best approach to find this to find the LCA of that number each and find the
