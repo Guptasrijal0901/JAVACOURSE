886f6f6e0a6aabf535377bfeab55be87e3dd24b5
Srijal Gupta <guptasrijal0901@gmail.com>
2025-02-16 00:12:58 +0530

    09

--- bTreeSumTree.java
+++ bTreeSumTree.java
@@ -1,5 +1,39 @@
+import org.w3c.dom.Node;
+
 public class bTreeSumTree {
+    static class Node {
+        int data;
+        Node left, right;
+
+        public Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static int transform(Node root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int leftChild = transform(root.left);
+        int rightChild = transform(root.right);
+
+        int data = root.data;
+
+        root.data = root.left.data + leftChild + rightChild + root.right.data;
+        return data;
+    }
+
     public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
 
     }
 
