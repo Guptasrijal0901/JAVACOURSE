d46af9c9bfee6f3b0df47dad64e0bd61b70be522
Srijal Gupta <guptasrijal0901@gmail.com>
2025-02-19 23:08:55 +0530

    lol

--- BSTintro.java
+++ BSTintro.java
@@ -7,3 +7,4 @@ public class BSTintro {
 // right subtree nodes > root
 // left and right subtree are also BST with no duplicates
 // Inorder traversal(left, root, right) of BST gives a sorted sequence
+// BST makes search efficient
