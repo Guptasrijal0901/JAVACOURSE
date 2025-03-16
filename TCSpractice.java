3be97ad043d6b29ef6ae8be6100d46a3e90fc3e2
Srijal Gupta <guptasrijal0901@gmail.com>
2025-03-16 22:12:10 +0530

    09

---
+++ TCSpractice.java
@@ -0,0 +1,17 @@
+public class TCSpractice {
+    // public static int nSum(int m, int n) {
+    // if (m > n) {
+    // return 0;
+    // }
+    // int sum = 0;
+    // for (int i = m; i <= n; i++) {
+    // sum = sum + i;
+    // }
+    // return sum;
+    // }
+
+    public static void main(String[] args) {
+        // System.out.println(nSum(0, 3));
+    }
+
+}
