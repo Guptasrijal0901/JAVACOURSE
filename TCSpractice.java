1d25ffce7651cd7519b251d9425e9f7a1f9d38bc
Srijal Gupta <guptasrijal0901@gmail.com>
2025-03-16 22:32:55 +0530

    lol

--- TCSpractice.java
+++ TCSpractice.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class TCSpractice {
     // public static int nSum(int m, int n) {
     // if (m > n) {
@@ -9,9 +11,25 @@ public class TCSpractice {
     // }
     // return sum;
     // }
+    public static int unique(int arr[]) {
+
+        if (arr.length == 0) {
+            return 0;
+        }
+        Arrays.sort(arr);
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] == arr[i + 1]) {
+                return arr[i];
+            }
+
+        }
+        return -1;
+    }
 
     public static void main(String[] args) {
         // System.out.println(nSum(0, 3));
+        int arr[] = { 1, 2, 6, 8, 7, 3, 2, 5, 2, 8, 5 };
+        System.out.println(unique(arr));
     }
 
 }
