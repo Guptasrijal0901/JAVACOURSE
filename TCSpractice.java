ba72d899ff99a85f3df5fef6c8220bc84bd2e848
Srijal Gupta <guptasrijal0901@gmail.com>
2025-03-25 18:43:24 +0530

    lol

--- TCSpractice.java
+++ TCSpractice.java
@@ -11,9 +11,27 @@ public class TCSpractice {
     // }
     // return sum;
     // }
+    public static int prime(int n) {
+        if (n <= 0  || n = 1) {
+            return 0;
+        }
+        for (int i = 1; i < n; i++) {
+            if (n % i == 0) {
+                return 1;
+                // System.out.println("number is no prime");
+                // int sum = 0;
+                // sum = sum + i;
+                // return sum;
+            }
+        }
+    }
+
     public static void main(String[] args) {
         // System.out.println(nSum(0, 3));
-        int arr[] = { 1, 2, 6, 8, 7, 3, 2, 5, 2, 8, 5 };
-        System.out.println(unique(arr));
+        // int arr[] = { 1, 2, 6, 8, 7, 3, 2, 5, 2, 8, 5 };
+        // System.out.println(unique(arr));
+        int n = 12;
+        System.out.println(prime(n));
+
     }
 }
